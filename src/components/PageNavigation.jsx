20a1435faa3b8bfca651327852facdc6d29f2afd
JaQwae <105896967+JaQwae@users.noreply.github.com>
2023-01-16 17:52:47 -0600

    Nav bar placeholder linked

---
+++ src/components/PageNavigation.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+// import Home from './pages/Home';
+// import Contact from './pages/Contact';
+
+function PageNavigation () {
+    return (
+        <h1>Page Nav Linked</h1>
+    )
+}
+
+export default PageNavigation;
