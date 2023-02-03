605643d1d1c85d3f777a4f130c796fcc68f041da
JaQwae <105896967+JaQwae@users.noreply.github.com>
2023-02-02 21:18:50 -0600

    contact form style styled

--- src/components/pages/Connect.jsx
+++ src/components/pages/Connect.jsx
@@ -4,28 +4,30 @@ const Connect = (theme) => {
             <h1 id='connect-page-title'>Connect With Me</h1>
             <form id={`connect-page-form-${theme.theme}`} className='connect-form-layout'>
                 <section class='contact-form-two-box-row-container'>
-                    <div class='contact-form-two-box-row-container-item'>
+                    <div class='form-sections'>
                         <input type='text' id='fname' name='fname'></input>
                         <label htmlFor='fname'>First name</label>
                     </div>
-                    <div class='contact-form-two-box-row-container-item'>
+                    <div class='form-sections'>
                         <input type='text' id='lname' name='lname'></input>
                         <label htmlFor='lname'>Last name</label>
                     </div>
                 </section>
                 <section class='contact-form-two-box-row-container'>
-                    <div class='contact-form-two-box-row-container-item'>
+                    <div class='form-sections'>
                         <input type='email' id='email' name='email'></input>
                         <label htmlFor='email'>Email</label>
                     </div>
-                    <div class='contact-form-two-box-row-container-item'>
+                    <div class='form-sections'>
                         <input type='text' id='phone-number' name='phone-number'></input>
                         <label htmlFor='phone-number'>Phone</label>
                     </div>
                 </section>
-                <input type='text' id='message' name='message'></input>
-                <label htmlFor='message'>Message</label>
-                <input type="submit" value="Submit"></input>
+                <section class='form-sections'>
+                    <input type='text' id='message' name='message'></input>
+                    <label htmlFor='message'>Message</label>
+                    <input type="submit" value="Submit"></input>
+                </section>
             </form>
         </div>
     )
