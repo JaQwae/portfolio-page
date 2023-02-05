51dc963710252137e64b2b7496fee64e4513c57e
JaQwae <105896967+JaQwae@users.noreply.github.com>
2023-02-05 14:45:04 -0600

    all field have submission logic to handle the submit button click

--- src/components/pages/Connect.jsx
+++ src/components/pages/Connect.jsx
@@ -4,8 +4,11 @@ const Connect = (theme) => {
     const [fname, setFname] = useState('');
     const [lname, setLname] = useState('');
     const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [message, setMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
+    // occur when an input box is clicked on
     const handleInputChange = (e) => {
         const { target } = e;
         const inputType = target.name;
@@ -21,32 +24,45 @@ const Connect = (theme) => {
         if (inputType === 'email') {
             setEmail(inputValue);
         };
+        if (inputType === 'phone') {
+            setPhone(inputValue);
+        };
+        if (inputType === 'message') {
+            setMessage(inputValue);
+        };
 
         console.log(inputType)
         console.log(inputValue)
     }
 
+    // occurs after the submit button is entered
     const handleFormSubmit = (e) => {
         e.preventDefault();
 
         if (!fname) {
             setErrorMessage('Please enter your first name.');
-            console.log(errorMessage);
             return;
         } else if (!lname) {
             setErrorMessage('Please enter your last name.');
-            console.log(errorMessage);
             return;
         } else if (!email){
             setErrorMessage('Please enter an email.');
-            console.log(errorMessage);
+            return;
+        } else if(!phone){
+            setErrorMessage('Please enter a phone number.');
+            return;
+        } else if (!message) {
+            setErrorMessage('Please enter a message.');
             return;
         } else {
             alert(`Hello ${fname} ${lname},thank you for submitting a connection form! I look forward to connecting with you!`);
 
+            // clears all inputs
             setFname('');
             setLname('');
             setEmail('');
+            setPhone('');
+            setMessage('');
             setErrorMessage('');
         }
     }
@@ -87,27 +103,31 @@ const Connect = (theme) => {
                             onChange= {handleInputChange}
                             className={`all-input-boxes all-input-boxes-${theme.theme}`}
                             type='email'
-                            value = {email}
+                            value={email}
                         ></input>
                         <label htmlFor='email'>Email</label>
                     </div>
                     <div className='form-sections'>
                         <input 
+                            name='phone' 
+                            id='phone'
+                            onChange= {handleInputChange}
+                            className={`all-input-boxes all-input-boxes-${theme.theme}`}
                             type='text'
-                            id='phone-number' 
-                            name='phone-number' 
-                            className={`all-input-boxes all-input-boxes-${theme.theme}`}>
-                        </input>
-                        <label htmlFor='phone-number'>Phone</label>
+                            value={phone}
+                        ></input>
+                        <label htmlFor='phone'>Phone</label>
                     </div>
                 </section>
                 <section className='form-sections'>
                     <textarea 
-                        type='text' 
-                        id='message' 
                         name='message' 
-                        className={`all-input-boxes all-input-boxes-${theme.theme}`}>
-                    </textarea>
+                        id='message'
+                        onChange= {handleInputChange}
+                        className={`all-input-boxes all-input-boxes-${theme.theme}`}
+                        type='text'
+                        value={message}
+                    ></textarea>
                     <label htmlFor='message'>Message</label>
                     <button 
                         onClick={handleFormSubmit}
