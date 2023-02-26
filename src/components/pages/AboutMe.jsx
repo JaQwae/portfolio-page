4d01c05189486b917345046cd1157e1caacc5e94
JaQwae <105896967+JaQwae@users.noreply.github.com>
2023-02-25 22:17:06 -0600

    map inserted on the about me page

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -7,13 +7,19 @@ const AboutMe = () => {
                 </section>
                 <section id='about-me-introduction-container'>
                 </section>
-                <section id='about-me-skill-section-container'>
-                </section>
                 <section id='about-me-education-container'>
                 </section>
                 <section id='about-me-interest-container'>
                 </section>
                 <section id='about-me-location-container'>
+                    <h3>Location</h3>
+                    <div className="about-me-location-content-container">
+                        <iframe className='location-map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d220448.6973139907!2d-97.89582413511677!3d30.30798270626543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8644b599a0cc032f%3A0x5d9b464bd469d57a!2sAustin%2C%20TX!5e0!3m2!1sen!2sus!4v1677383731672!5m2!1sen!2sus" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                        <div className="location-information">
+                            <h6>Located in: Austin</h6>
+                            <h6>Open to Relocate: Yes</h6>
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
--- src/components/portfolio-data/data.js
+++ src/components/portfolio-data/data.js
@@ -3,7 +3,8 @@ export const data = [
         id: 1,
         category: "front-end",
         title: "project-1",
-        image: "image-1"
+        image: "image-1",
+        gitHub: "link-1"
     },
     {
         id: 2,
@@ -16,5 +17,35 @@ export const data = [
         category: "full-stack",
         title: "project-3",
         image: "image-3"
+    },
+    {
+        id: 4,
+        category: "full-stack",
+        title: "project-4",
+        image: "image-3"
+    },
+    {
+        id: 5,
+        category: "front-end",
+        title: "project-5",
+        image: "image-5"
+    },
+    {
+        id: 6,
+        category: "front-end",
+        title: "project-6",
+        image: "image-6"
+    },
+    {
+        id: 7,
+        category: "front-end",
+        title: "project-7",
+        image: "image-7"
+    },
+    {
+        id: 8,
+        category: "front-end",
+        title: "project-8",
+        image: "image-8"
     }
 ]
