102462031d2e6fc76ac32fe2eb2a63a01285530c
JaQwae <105896967+JaQwae@users.noreply.github.com>
2023-02-26 18:58:32 -0600

    added project and styling to portfolio

--- src/components/portfolio-data/data.js
+++ src/components/portfolio-data/data.js
@@ -4,47 +4,52 @@ export const data = [
         category: "front-end",
         title: "Work-Day Scheduler",
         image: "https://github.com/JaQwae/workday-scheduler/raw/main/images/workdayScreenshot.png",
-        altText: "Image of Work Day Application",
+        altText: "Work Day Application",
         deployedLink: "https://jaqwae.github.io/workday-scheduler/",
         gitHubLink: "https://github.com/JaQwae/workday-scheduler"
     },
     {
         id: 2,
         category: "back-end",
-        title: "project-2",
+        title: "Social Network API",
         image: "image-2",
+        altText: "Social Network API Application",
         deployedLink: "d-link-2",
-        gitHubLink: "link-2"
+        gitHubLink: "https://github.com/JaQwae/Social-Network-API"
     },
     {
         id: 3,
         category: "full-stack",
-        title: "project-3",
+        title: "Note Taker",
         image: "image-3",
+        altText: "Note Taker Application",
         deployedLink: "d-link-3",
-        gitHubLink: "link-3"
+        gitHubLink: "https://github.com/JaQwae/Note-Taker"
     },
     {
         id: 4,
         category: "full-stack",
         title: "project-4",
         image: "image-4",
+        altText: "alt-text-4",
         deployedLink: "d-link-4",
         gitHubLink: "link-4"
     },
     {
         id: 5,
         category: "front-end",
-        title: "project-5",
-        image: "image-5",
-        deployedLink: "d-link-5",
-        gitHubLink: "link-5"
+        title: "Weather Dashboard",
+        image: "https://github.com/JaQwae/weatherDashboard/raw/main/images/weatherDashBoardScreenBefore.png",
+        altText: "Weather Dashboard Application",
+        deployedLink: "https://jaqwae.github.io/weatherDashboard/",
+        gitHubLink: "https://github.com/JaQwae/weatherDashboard"
     },
     {
         id: 6,
         category: "front-end",
         title: "project-6",
         image: "image-6",
+        altText: "alt-text-6",
         deployedLink: "d-link-6",
         gitHubLink: "link-6"
     },
@@ -53,6 +58,7 @@ export const data = [
         category: "front-end",
         title: "project-7",
         image: "image-7",
+        altText: "alt-text-7",
         deployedLink: "d-link-7",
         gitHubLink: "link-7"
     },
@@ -61,6 +67,7 @@ export const data = [
         category: "front-end",
         title: "project-8",
         image: "image-8",
+        altText: "alt-text-8",
         deployedLink: "d-link-8",
         gitHubLink: "link-8"
     }
