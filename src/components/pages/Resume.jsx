1b4dabc41be79ed424ae99838147c5c5080d6e74
JaQwae <105896967+JaQwae@users.noreply.github.com>
2024-04-06 14:14:15 -0700

    updated resume

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import resume from "../../assets/images/Tech Resume 3122023.docx.pdf";
+import resume from "../../assets/images/Tech Resume 03272024.pdf";
 
 const Resume = (theme) => {
     return (
